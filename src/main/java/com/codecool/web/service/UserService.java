f55adce5dc4d0ebdca66bb7c1f3c68eba83e4ce7
daviddude <uszkai.d@gmail.com>
2018-03-21 16:58:03 +0100

    added Userservice class

---
+++ src/main/java/com/codecool/web/service/UserService.java
@@ -0,0 +1,41 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public final class UserService {
+
+    public void changeUserAttr(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User tempUser = (User) req.getSession().getAttribute("logged");
+        String userName = tempUser.getName();
+        String userRole = tempUser.getRole();
+
+        if (req.getParameter("name").equals(userName) && req.getParameter("role") == null) {
+            req.getRequestDispatcher("main.jsp").forward(req, resp);
+
+        } else if (req.getParameter("role").equalsIgnoreCase(userRole)) {
+            req.getRequestDispatcher("main.jsp").forward(req, resp);
+
+        } else if (!req.getParameter("name").equals(userName) && req.getParameter("role") == null || req.getParameter("role").equalsIgnoreCase(userRole) && !req.getParameter("name").equals(userName)) {
+            tempUser.setName(req.getParameter("name"));
+            req.setAttribute("name", tempUser);
+            req.getRequestDispatcher("main.jsp").forward(req, resp);
+
+        } else if (!req.getParameter("role").equalsIgnoreCase(userRole) && req.getParameter("name").equals(userName)) {
+            tempUser.setRole(req.getParameter("role"));
+            req.setAttribute("role", tempUser);
+            req.getRequestDispatcher("main.jsp").forward(req, resp);
+
+        } else if (!req.getParameter("role").equals(userRole) && !req.getParameter("name").equals(userName)) {
+            tempUser.setRole(req.getParameter("role"));
+            req.setAttribute("role", tempUser);
+            tempUser.setName(req.getParameter("name"));
+            req.setAttribute("name", tempUser);
+            req.getRequestDispatcher("main.jsp").forward(req, resp);
+        }
+    }
+}
--- src/main/java/com/codecool/web/servlet/EditUserServlet.java
+++ src/main/java/com/codecool/web/servlet/EditUserServlet.java
@@ -2,6 +2,7 @@ package com.codecool.web.servlet;
 
 import com.codecool.web.model.User;
 import com.codecool.web.service.LoginService;
+import com.codecool.web.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,32 +17,10 @@ public class EditUserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User tempUser = (User)req.getSession().getAttribute("logged");
-        String userName = tempUser.getName();
-        String userRole = tempUser.getRole();
-
-        if (req.getParameter("name").equals(userName) && req.getParameter("role") == null) {
-            req.getRequestDispatcher("main.jsp").forward(req, resp);
-
-        } else if (req.getParameter("role").equalsIgnoreCase(userRole)) {
-            req.getRequestDispatcher("main.jsp").forward(req, resp);
-
-        } else if (!req.getParameter("name").equals(userName) && req.getParameter("role") == null || req.getParameter("role").equalsIgnoreCase(userRole) && !req.getParameter("name").equals(userName)) {
-            tempUser.setName(req.getParameter("name"));
-            req.setAttribute("name", tempUser);
-            req.getRequestDispatcher("main.jsp").forward(req, resp);
-
-        } else if (!req.getParameter("role").equalsIgnoreCase(userRole) && req.getParameter("name").equals(userName)) {
-            tempUser.setRole(req.getParameter("role"));
-            req.setAttribute("role", tempUser);
-            req.getRequestDispatcher("main.jsp").forward(req, resp);
-
-        } else if (!req.getParameter("role").equals(userRole) && !req.getParameter("name").equals(userName)) {
-            tempUser.setRole(req.getParameter("role"));
-            req.setAttribute("role", tempUser);
-            tempUser.setName(req.getParameter("name"));
-            req.setAttribute("name", tempUser);
-            req.getRequestDispatcher("main.jsp").forward(req, resp);
-        }
+        UserService us = new UserService();
+        us.changeUserAttr(req, resp);
     }
+
+
+
 }
