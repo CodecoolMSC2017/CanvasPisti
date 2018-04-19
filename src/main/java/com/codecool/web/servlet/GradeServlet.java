7b3388f3d80906fc71a68e70c9c72ec5d08f9ecb
daviddude <uszkai.d@gmail.com>
2018-04-19 13:13:37 +0200

    grades needs to be fixed

--- src/main/java/com/codecool/web/servlet/GradeServlet.java
+++ src/main/java/com/codecool/web/servlet/GradeServlet.java
@@ -1,28 +1,43 @@
 package com.codecool.web.servlet;
 
 
+import com.codecool.web.dao.PageDao;
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.dao.database.DatabasePageDao;
+import com.codecool.web.dao.database.DatabaseUserDao;
 import com.codecool.web.model.AssignmentPage;
-import com.codecool.web.model.Singletondb;
 import com.codecool.web.model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Map;
 
 @WebServlet("/grades")
-public class GradeServlet extends HttpServlet {
+public class GradeServlet extends AbstractServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Singletondb db = Singletondb.getInstance();
-        User tempUser = (User)req.getSession().getAttribute("logged");
-        ArrayList<AssignmentPage> pagez = db.getSubmissions().get(tempUser);
-        String userName = tempUser.getName();
-        req.setAttribute("username", userName);
-        req.setAttribute("userassigns", pagez);
-        req.getRequestDispatcher("grades.jsp").forward(req, resp);
+        try(Connection connection = getConnection(req.getServletContext())){
+            PageDao pageDao = new DatabasePageDao(connection);
+            User tempUser = (User) req.getSession().getAttribute("logged");
+            Map<User,ArrayList<AssignmentPage>> myMap = pageDao.getSubmissionList();
+            for (Map.Entry<User, ArrayList<AssignmentPage>> entry : myMap.entrySet()) {
+                if (entry.getKey().getEmail().equals(tempUser.getEmail())){
+                    ArrayList<AssignmentPage> pagez = myMap.get(entry.getKey());
+                    req.setAttribute("username",entry.getKey().getEmail() );
+                    req.setAttribute("userassigns", pagez);
+
+                }
+            }
+            req.getRequestDispatcher("grades.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
